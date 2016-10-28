507cd16383b41a8da965cced7f38f90311721089
Willem Zwart <willem.zwart@gmail.com>
2016-10-28 16:36:38 +0200

    initial

---
+++ configure_r10k.pp
@@ -0,0 +1,25 @@
+######         ######
+##  Configure R10k ##
+######         ######
+
+##  This manifest requires the zack/R10k module and will attempt to
+##  configure R10k according to my blog post on directory environments.
+##  Beware! (and good luck!)
+
+## version should correspond to the Gem version zack-r10k is tested against:
+## https://forge.puppetlabs.com/zack/r10k
+## (e.g. 1.5.1 for recent module versions, expected to be 2.0.3 at the next major zack-r10k release)
+
+class { 'r10k':
+  version           => '2.4.3',
+  sources           => {
+    'puppet' => {
+      'remote'  => 'https://github.com/zwartw/puppet_repository.git',
+      'basedir' => "${::settings::confdir}/environments",
+      'prefix'  => false,
+    }
+  },
+  ## purgedirs is deprecated, but may be needed for older versions (see https://github.com/acidprime/r10k/pull/84)
+  ## purgedirs         => ["${::settings::confdir}/environments"],
+  manage_modulepath => false,
+}
